55926a75e73ab5b0558c313c684ca785e9e7e049
dls4585 <dls4585@naver.com>
2021-02-03 16:52:10 +0900

    Category, Service 리포/서비스

--- src/main/java/ssodam/clone/domain/Post.java
+++ src/main/java/ssodam/clone/domain/Post.java
@@ -2,6 +2,7 @@ package ssodam.clone.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -22,21 +23,39 @@ public class Post {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id") // 우선 다대일로 설정, 후에 논의 필요
+    @Column(nullable = false)
     private Category category;
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
     private List<Comment> comments = new ArrayList<>();
 
+    @ColumnDefault("0")
+    private int commentsNo;
+
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String contents;
 
+    @ColumnDefault("0")
     private int like;
+    @ColumnDefault("0")
     private int dislike;
+    @ColumnDefault("0")
     private int scrap;
+    @ColumnDefault("0")
     private int visit;
 
     private LocalDateTime createDate;
     private LocalDateTime updateDate;
 
+    // == 생성 메서드 == //
+
+    public void createPost(Member member, Category category, String title, String contents) {
+        this.member = member;
+        this.category = category;
+        this.title = title;
+        this.contents = contents;
+    }
 
 }
--- src/main/java/ssodam/clone/repository/PostRepository.java
+++ src/main/java/ssodam/clone/repository/PostRepository.java
@@ -1,56 +1,15 @@
 package ssodam.clone.repository;
 
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import ssodam.clone.domain.Category;
 import ssodam.clone.domain.Post;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class PostRepository {
-
-    private EntityManager em;
-
-    /**
-     * create
-     */
-    public void save(Post post) {
-        em.persist(post);
-    }
-
-    /**
-     * read
-     */
-
-    public Post findOne(Long id) {
-        return em.find(Post.class, id);
-    }
-
-    public List<Post> findAll() {
-        return em.createQuery("select p from Post p", Post.class)
-                .getResultList();
-    }
-
-    public  List<Post> findByMember(Member member) {
-        return em.createQuery("select p from Post p where p.member = :member", Post.class)
-                .setParameter("member", member)
-                .getResultList();
-    }
-
-    /**
-     * update
-     */
-
-
-
-    /**
-     * delete
-     */
-    public void delete(Long id) {
-        em.createQuery("delete from Post p where p.id =: id", Post.class)
-                .setParameter("id", id);
-    }
+public interface PostRepository {
+    public void save(Post post);
+    public Post findOne(Long id);
+    public List<Post> findAll();
+    public  List<Post> findByMember(Member member);
+    public List<Post> findByCategory(Category category);
+    public void delete(Long id);
 }
