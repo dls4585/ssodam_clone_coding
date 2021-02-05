f96342f63844880e4f02fbd2d65d7cf92d3f9a5a
dls4585 <dls4585@naver.com>
2021-02-05 19:53:04 +0900

    원상복귀 ㅠㅠ

---
+++ src/main/java/ssodam/ssodam/repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+package ssodam.ssodam.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ssodam.ssodam.domain.Category;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    public Category findByName(String categoryName);
+
+}
