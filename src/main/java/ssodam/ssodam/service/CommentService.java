e699f042acf4f46547b18f31179eeb502e34af35
codingSJ <seojoon.hong94@gmail.com>
2021-02-16 23:53:35 +0900

    2/16

--- src/main/java/ssodam/ssodam/service/CommentService.java
+++ src/main/java/ssodam/ssodam/service/CommentService.java
@@ -1,13 +1,12 @@
 package ssodam.ssodam.service;
 
 import ssodam.ssodam.domain.Comment;
+import ssodam.ssodam.domain.Member;
+import ssodam.ssodam.domain.Post;
+
+import java.util.List;
 
 public interface CommentService {
-
-//    findOne
-//    findAll
-//    save
-
     /**
      * 댓글 생성
      */
@@ -18,7 +17,6 @@ public interface CommentService {
      */
     Comment writeSubcomment(Long memberId, Long superCommentId, String content);
 
-
     /**
      * 댓글 수정
      */
@@ -28,4 +26,24 @@ public interface CommentService {
      * 댓글 삭제
      */
     void deleteComment(Long memberId, Long postId, Long commentId);
+
+    /**
+     * 댓글 찾기
+     */
+    Comment findOne(Long commentId);
+
+    /**
+     * 모든 댓글 찾기
+     */
+    List<Comment> findAll();
+
+    /**
+     * 멤버 댓글 찾기
+     */
+    List<Comment> findByMember(Member member);
+
+    /**
+     * 포스트의 댓글 찾기
+     */
+    List<Comment> findByPost(Post post);
 }
